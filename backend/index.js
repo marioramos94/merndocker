88992875ba67f4c73bd5334b022eb02d6c43e359
mario <mariosergio.rs94@gmail.com>
2019-03-25 14:54:32 -0500

    front y back2

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
 app.get('/', (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
-    if (err) {
+        if (err) {
       res.status(500).send('💥 BOOM 💥: ' + err);
     } else {
       res.send('Me conecté a la DB! 😎');
@@ -17,5 +17,60 @@ app.get('/', (req, res) => {
     }
   });
 });
+
+// endpoint para redirigir al front
+app.get('/home', (req, res) => {
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+        if (err) {
+      res.status(500).send('💥 BOOM 💥: ' + err);
+    } else {
+      res.send('Me conecté a la DB! 😎');
+      db.close();
+    }
+  });
+});
+
+
+
+// endpoint para leer todos los productos 
+app.get('/products', (req, res) => {
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+        if (err) {
+      res.status(500).send('💥 BOOM 💥: ' + err);
+    } else {
+      res.send('Me conecté a la DB! 😎');
+      db.close();
+    }
+  });
+});
+
+//endpoint para crear los productos
+
+app.post('/product', (req, res) => {
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+        if (err) {
+      res.status(500).send('💥 BOOM 💥: ' + err);
+    } else {
+      res.send('Me conecté a la DB! 😎');
+      db.close();
+    }
+  });
+});
+
+
+//endpoint para eliminar los productos 
+app.delete('/product', (req, res) => {
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
+        if (err) {
+      res.status(500).send('💥 BOOM 💥: ' + err);
+    } else {
+      res.send('Me conecté a la DB! 😎');
+      db.close();
+    }
+  });
+});
+
+
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
