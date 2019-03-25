6957919ab70198b64212b5470c2f2f903ef87746
mario <mariosergio.rs94@gmail.com>
2019-03-25 17:51:31 -0500

    front y back21

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ app.post('/product', (req, res) => {
     console.log(mongoUrl)
     if (err);
     var dbo = db.db("store");
-    var myobj = { name: "Company Inc", address: "Highway 37" };
+    var myobj = req.body;
     dbo.collection("products").insertOne(myobj, function(err, res) {
       if (err) throw err;
       console.log("Product Added ");
